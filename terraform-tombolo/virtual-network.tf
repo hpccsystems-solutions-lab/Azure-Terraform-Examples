2b7d38d2d8822fa5f46dfce8ea4c1684509c56dd
arjunachala <arjunachala@gmail.com>
2022-02-04 16:30:55 -0500

    updated comments

--- terraform-tombolo/virtual-network.tf
+++ terraform-tombolo/virtual-network.tf
@@ -1,3 +1,11 @@
+/*
+  The Virual Network to is used to encapsulate the resources within a resource group.
+  1) Isolation
+  2) Organization
+  3) Rules based Ingress and Egress 
+  4) Peering to connect to other VNets or On Prem Express Route
+*/
+
 module "virtual_network" {
   source = "github.com/Azure-Terraform/terraform-azurerm-virtual-network"
 
@@ -14,7 +22,7 @@ module "virtual_network" {
      Organize the resources into subnets. 
 
      1) Subnets encapsulates a set of resources and reserves IPs for these resources 
-     2) Subnets help enforce network security rules
+     2) Subnets help enforce network security rules using network securty groups
      3) A dedicated Subnet with enough IPs is a requirement for PaaS VNet integration
      4) Expanding a Subnet IP space will require the creation of a new Subnet, copying the existing resources from the old subnet to the new one and deleting the old subnet
 
@@ -47,7 +55,7 @@ module "virtual_network" {
       }   
     }
 
-    
+    //Subnet reserved for Private Endpoints
     mysql-db = {
       cidrs = ["10.1.0.96/27"]//10.1.0.96 - 10.1.0.127 (32 addresses. With 5 reserved for Azure)
       enforce_private_link_endpoint_network_policies  = true
@@ -56,12 +64,16 @@ module "virtual_network" {
   }      
 }
 
+/* Network Security Group associated with the App Gateway subnet */
+
 resource "azurerm_network_security_group" "app-gateway-nsg" {
   name                = "app-gateway-nsg"
   location            = module.resource-group.location
   resource_group_name = module.resource-group.name  
 }
 
+/* Security rule for app-gateway-nsg to allow http access to the Gateway subnet from LNRS network */
+//NOTE: The order of the parameters in the rule is important
 resource "azurerm_network_security_rule" "lnrsvpnallowhttpaccess" {
   name                        = "LNRSVPNAllowHttp"
   priority                    = 100
@@ -75,6 +87,8 @@ resource "azurerm_network_security_rule" "lnrsvpnallowhttpaccess" {
   resource_group_name         = module.resource-group.name
   network_security_group_name = azurerm_network_security_group.app-gateway-nsg.name
 }
+
+/* Security rule for app-gateway-nsg to allow http access to the Gateway subnet for Gateway health check */
 
 resource "azurerm_network_security_rule" "allowgatewaymanager" {
   name                        = "AllowGatewayManagerAccess"
@@ -90,6 +104,7 @@ resource "azurerm_network_security_rule" "allowgatewaymanager" {
   network_security_group_name = azurerm_network_security_group.app-gateway-nsg.name
 }
 
+/* Associate the NSG to the Gateway Subnet */
 resource "azurerm_subnet_network_security_group_association" "app-gateway-subnet-nsg" {
   subnet_id                 = module.virtual_network.subnet["app-gateway"].id
   network_security_group_id = azurerm_network_security_group.app-gateway-nsg.id
